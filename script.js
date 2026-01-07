88ec00d8dc2fbba282e410ae474a631de6b99577
Abubakar <abubakarkhan909@gmail.com>
2026-01-07 16:51:30 +0500

    add new code

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 alert("JS Connected Successfully!");
-
+{
 let name;
 name = "Husnain Muavia";
-console.log("My name is" + "" + name);
+console.log("My name is" + " " + name);
+}
+
+{
+    let firstName = "Husnain";
+    let lastName = "Muavia";
+    let fullName = firstName + " " + lastName;
+    console.log("My full name is " + fullName);
+}
