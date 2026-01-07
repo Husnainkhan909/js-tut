467cd14a82ab49d86c8ade7b6943651114e0bd1e
Abubakar <abubakarkhan909@gmail.com>
2026-01-07 17:46:22 +0500

    add new

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 alert("JS Connected Successfully!");
+
+ function toggleMenu() {
+            const menu = document.getElementById("mobileMenu");
+            if (menu.style.display === "flex") {
+                menu.style.display = "none";
+            } else {
+                menu.style.display = "flex";
+            }
+        }
 {
 let name;
 name = "Husnain Muavia";
@@ -11,3 +20,13 @@ console.log("My name is" + " " + name);
     let fullName = firstName + " " + lastName;
     console.log("My full name is " + fullName);
 }
+
+const student ={
+    fullName: "Husnain Muavia",
+    age : 20,
+    rollno : 26844,
+    cgpa : 3.4,
+    ispass : true,
+}
+console.log(student);
+console.log("student.age");
